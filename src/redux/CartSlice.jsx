3211c0dafe74b9e30bcc0093d7c30d3091867e07
ehernandezv-svg <e.hernandezv@lanuevaamerica.edu.co>
2026-04-26 17:41:06 -0500

    Refactor cart slice actions and rename functions

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -8,24 +8,23 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action) => {
+    addItem: (state, action) => {
       const item = state.items.find(i => i.id === action.payload.id);
       if (item) item.quantity++;
       else state.items.push({ ...action.payload, quantity: 1 });
     },
-    removeFromCart: (state, action) => {
+    removeItem: (state, action) => {
       state.items = state.items.filter(i => i.id !== action.payload);
     },
-    increaseQuantity: (state, action) => {
-      const item = state.items.find(i => i.id === action.payload);
-      if (item) item.quantity++;
-    },
-    decreaseQuantity: (state, action) => {
-      const item = state.items.find(i => i.id === action.payload);
-      if (item && item.quantity > 1) item.quantity--;
+    updateQuantity: (state, action) => {
+      const { id, amount } = action.payload;
+      const item = state.items.find(i => i.id === id);
+      if (item && item.quantity + amount > 0) {
+        item.quantity += amount;
+      }
     }
   }
 });
 
-export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
